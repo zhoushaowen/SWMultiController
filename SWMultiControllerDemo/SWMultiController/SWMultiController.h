c365284bd820c6b459deacf4570a0c834ae01bc3
周少文 <348345883@qq.com>
2020-02-17 14:16:13 +0800

    暴露subViewControllers给外部调用

--- SWMultiControllerDemo/SWMultiController/SWMultiController.h
+++ SWMultiControllerDemo/SWMultiController/SWMultiController.h
@@ -43,6 +43,9 @@ NS_ASSUME_NONNULL_BEGIN
  @param selectedIndex 选择的索引
  */
 - (void)reloadWithSubViewControllers:(NSArray<UIViewController *> *)subViewControllers selectedIndex:(NSUInteger)selectedIndex;
+
+/// 子控制器
+@property (nonatomic,copy,readonly) NSArray<UIViewController *> *subViewControllers;
 /**
  选中了某个控制器的回调
  */
